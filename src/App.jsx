c381f25b925d758d77587da78eb255096db4172b
CC Ly <vanessly@umich.edu>
2024-04-03 20:32:02 -0400

    Change favicon

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './assets/styles/App.css';
 import Header from './components/Header'
 import About from './components/About'
 import Grid from './components/Grid'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <About />
       <Grid />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// About.jsx
+import React, { useState, useEffect } from "react";
+import '../assets/styles/Footer.css';
+
+function Footer() {
+
+  return (
+    <div className="Footer">
+        <h1>hi</h1>
+        <h1>deez</h1>
+    </div>
+  );
+}
+
+export default Footer;
