59e5b65bdff20b49eeacc192ec45a6126994dc0c
CC Ly <vanessly@umich.edu>
2024-04-11 20:00:19 -0400

    Fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <a href="#main" tabIndex="0" className="skip">Skip to Main Content</a>
-      <BrowserRouter>
+      <BrowserRouter basename="/si339finalproject">
         <Header />
         <Routes>
           <Route path="/" element={<Grid />} />
